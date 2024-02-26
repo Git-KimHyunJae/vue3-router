0b19cae763b9e98294a6bb445545c1d1015e1dd4
Kim Hyun Jae <namro7633@gmail.com>
2024-02-26 19:51:05 +0900

    initcommit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About View</h2>
+    <p>{{ $route.path }}</p>
+    <button class="btn btn-primary" @click="goHome">Home 으로 이동</button>
+  </div>
+</template>
+
+<script setup>
+import {useRoute,useRouter} from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+
+const goHome = () => {
+  router.push('/')
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Home View</h2>
+    <p>{{ $route.path }}</p>
+    <button class="btn btn-primary" @click="goAboutPage">About 으로 이동</button>
+  </div>
+</template>
+
+<script setup>
+import { useRoute,useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+const goAboutPage = () => {
+  router.push('/About')
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
